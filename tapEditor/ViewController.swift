2173e55d288726dfe36436b9c9054eb38321f2cf
zhoujun0920 <zhoujun199009@hotmail.com>
2015-07-23 14:28:23 -0500

    Initial Commit

---
+++ tapEditor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tapEditor
+//
+//  Created by Jun Zhou on 7/23/15.
+//  Copyright (c) 2015 Jun Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
